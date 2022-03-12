d10dd33014d9e7048af051626922872b9858ec95
unknown <AdiTheZero@gmail.com>
2022-03-12 20:51:06 +0530

    Add

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App basename={process.env.PUBLIC_URL} />
   </React.StrictMode>,
   document.getElementById('root')
 );
